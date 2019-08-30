364bec5695ececc06843577a9f475377fabf457c
Alexey Shishov <alexey.shishov@requestum.com>
2019-08-30 12:55:20 +0300

    Added utils, glide, permission, swipe controller for recyclerview items, base adapters and a roundimageview

---
+++ app/src/main/java/com/yourgains/mvvmdaggerkotlintemplate/presentation/adapters/OnItemClickListener.kt
@@ -0,0 +1,6 @@
+package com.yourgains.mvvmdaggerkotlintemplate.presentation.adapters
+
+interface OnItemClickListener<T> {
+
+    fun onItemClick(item: T)
+}
---
+++ app/src/main/java/com/yourgains/mvvmdaggerkotlintemplate/presentation/adapters/controllers/OnRightButtonsListener.kt
@@ -0,0 +1,8 @@
+package com.yourgains.mvvmdaggerkotlintemplate.presentation.adapters.controllers
+
+interface OnRightButtonsListener {
+
+    fun onDeleteClicked(position: Int)
+
+    fun onEditClicked(position: Int)
+}
